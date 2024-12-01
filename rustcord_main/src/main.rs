b8aa4cbd9c78dd549fa338132aabe24ac23830b4
sappy <185996433+sappyduckie@users.noreply.github.com>
2024-11-30 20:56:53 -0800

    Update main.rs

--- rustcord_main/src/main.rs
+++ rustcord_main/src/main.rs
@@ -5,10 +5,10 @@
 // This program is free software; you can redistribute it and/or modify    │
 // it under the terms of the GNU General Public License as published by    │
 // the Free Software Foundation; either version 2 of the License, or       │
-// (at your option) any later version.                                   へ  ♡
-//                                                                   ૮ >  <) 
-// This program is distributed in the hope that it will be useful,    / ⁻ ៸|
-// but WITHOUT ANY WARRANTY; without even the implied warranty of  乀(ˍ, ل ل 
+// (at your option) any later version.                                     へ ♡
+//                                                                     ૮ >  <) 
+// This program is distributed in the hope that it will be useful,      / ⁻ ៸|
+// but WITHOUT ANY WARRANTY; without even the implied warranty of    乀(ˍ, ل ل 
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           │
 // GNU General Public License for more details.                            │
 //                                                                         │
